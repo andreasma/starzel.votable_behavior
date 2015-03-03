f7b86eba939e7d63fd55bd10e4512897ff2bb9ff
Patrick Gerken <do3cc@patrick-gerken.de>
2015-03-03 11:55:50 +0100

    Well, this didnt work

--- src/starzel/votable_behavior/__init__.py
+++ src/starzel/votable_behavior/__init__.py
@@ -4,3 +4,6 @@
 from zope.i18nmessageid import MessageFactory
 
 _ = MessageFactory('starzel.votable_behavior')
+
+DoVote = 'starzel.votable_behavior: Do Vote'
+ViewVote = 'starzel.votable_behavior: View Vote'
--- src/starzel/votable_behavior/browser/vote.py
+++ src/starzel/votable_behavior/browser/vote.py
@@ -1,6 +1,8 @@
 from Products.Five.browser import BrowserView
 from starzel.votable_behavior.interfaces import IVotable
 from starzel.votable_behavior.interfaces import IVoting
+from zope.event import notify
+
 
 class Vote(BrowserView):
     def __call__(self, rating=None):
@@ -9,10 +11,13 @@ class Vote(BrowserView):
 
         voting = IVoting(self.context)
         voting.vote(rating, self.request)
+        notify(ObjectModifiedEvent(self.context, "A vote has been submitted"))
         return 'success'
 
 class ClearVotes(BrowserView):
     def __call__(self):
         voting = IVoting(self.context)
         voting.clear()
+        notify(ObjectModifiedEvent(self.context,
+                                   "All votes have been removed"))
         return 'sucess'
